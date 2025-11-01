a3133470eaa8b76cdb662933c0c78c587f346f84
SW-Consultant <klassgleb45@gmail.com>
2025-11-01 09:52:00 +0500

    Update server.py

--- server.py
+++ server.py
@@ -47,5 +47,6 @@ def create_payment():
     approve_link = next(link["href"] for link in payment_data["links"] if link["rel"] == "approve")
     return jsonify({"payment_url": approve_link})
 
-if __name__ == "__main__":
-    app.run(port=5000)
+if _name_ == "_main_":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
