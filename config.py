70a05d367be450ae86ba5b29805f6e7f8889e629
SW-Consultant <klassgleb45@gmail.com>
2025-11-01 12:14:47 +0500

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 PAYPAL_CLIENT_ID = os.environ.get("AVKqlu_sC9lUqMkwAgtKt9mPfFwx9szqd73ACEX_Hv3RLc8lfs03mf30vbyuk9Pq7_v6V7VVa7_6yrNw")
 PAYPAL_SECRET = os.environ.get("EEOeZMU3LkoWRswuXmYprrj-xwSvezbyUB-dUO-Ee7wPJ0P7HTEy6gEHK7CEnGbclH1pEEWutTerr3Sz")
-WEBHOOK_SECRET_KEY = os.environ.get("11012012gg479Gleb")
+SECRET_KEY = os.environ.get("11012012gg479Gleb")
